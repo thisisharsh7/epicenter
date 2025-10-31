13c3981a1a7310203f8acb7ec926d8605366bf60
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-10-30 23:42:09 -0500

    feat: add grayscale desaturation and transparency to collapsed vertical nav

--- apps/whispering/src/routes/(app)/verticalnav/+layout/VerticalNav.svelte
+++ apps/whispering/src/routes/(app)/verticalnav/+layout/VerticalNav.svelte
@@ -85,7 +85,7 @@
 	collapsible="icon"
 	side="left"
 	variant="sidebar"
-	class="overflow-x-hidden [&_[data-slot='sidebar-inner']]:group-data-[collapsible=icon]:opacity-40 [&_[data-slot='sidebar-inner']]:group-data-[collapsible=icon]:hover:opacity-100 [&_[data-slot='sidebar-inner']]:transition-opacity [&_[data-slot='sidebar-inner']]:duration-200"
+	class="overflow-x-hidden [&_[data-slot='sidebar-inner']]:group-data-[collapsible=icon]:[filter:grayscale(50%)] [&_[data-slot='sidebar-inner']]:group-data-[collapsible=icon]:opacity-80 [&_[data-slot='sidebar-inner']]:group-data-[collapsible=icon]:hover:[filter:grayscale(0%)] [&_[data-slot='sidebar-inner']]:group-data-[collapsible=icon]:hover:opacity-100 [&_[data-slot='sidebar-inner']]:transition-[filter,opacity] [&_[data-slot='sidebar-inner']]:duration-200"
 >
 	<Sidebar.Rail />
 	<Sidebar.Header
